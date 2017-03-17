9c6669a357af60281c1e97a57ba45726e0516406
ceraph <elias.m@protonmail.com>
2017-03-17 18:42:18 +0100

    Added basic XSD grammar.

--- src/yars/configuration/data/DataMuscleActuator.cpp
+++ src/yars/configuration/data/DataMuscleActuator.cpp
@@ -1,8 +1,14 @@
 #include "DataMuscleActuator.h"
 
 #include "yars/configuration/data/DataActuator.h"
+#include "yars/configuration/xsd/specification/XsdSpecification.h"
 
-DataMuscleActuator::DataMuscleActuator(DataNode* parent)
+#define YARS_STRING_MUSCLE_DEFINITION "slider" DIVIDER "definition"
+#define YARS_STRING_ACTUATOR_MODE_DEFINITION "actuator" \
+  DIVIDER "mode" DIVIDER "active" DIVIDER "passive" DIVIDER "definition"
+#define YARS_STRING_FORCE_DEFINITION "actuator_force" DIVIDER "definition"
+
+DataMuscleActuator::DataMuscleActuator(DataNode *parent)
   : DataActuator(parent, DATA_ACTUATOR_MUSCLE)
 {
 }
@@ -19,7 +25,7 @@ std::string DataMuscleActuator::source()
 {
 }
 
-std::string DataMuscleActuator::destination()
+std::string DataMuscleActuator::destination() const
 {
 }
 
@@ -92,3 +98,33 @@ DataActuator* DataMuscleActuator::_copy()
 {
 }
 
+void DataMuscleActuator::createXsd(XsdSpecification& spec)
+{
+  // TODO: smart pointer?
+  XsdSequence *muscleDefinition = new XsdSequence(YARS_STRING_MUSCLE_DEFINITION);
+  muscleDefinition->add(NA("name", "xs:string", false));
+  muscleDefinition->add(NA("type", "xs:string", false));
+  muscleDefinition->add(NA("mode", YARS_STRING_ACTUATOR_MODE_DEFINITION,
+    true));
+  muscleDefinition->add(NE("source",      YARS_STRING_NAME_DEFINITION, 1, 1));
+  muscleDefinition->add(NE("destination", YARS_STRING_NAME_DEFINITION, 1, 1));
+  muscleDefinition->add(NE("force-length_model",
+    "force-length_model_definition", 1, 1));
+  muscleDefinition->add(NE("force-velocity_model",
+    "force-velocity_model_definition", 1, 1));
+  spec.add(muscleDefinition);
+
+  XsdEnumeration *forceLengthModelDef = new XsdEnumeration(
+    "force-length_model_definition","xs:string");
+  forceLengthModelDef->add("constant");
+  forceLengthModelDef->add("linear");
+  forceLengthModelDef->add("hill");
+  spec.add(forceLengthModelDef);
+
+  XsdEnumeration *forceVelocityModelDef = new XsdEnumeration(
+    "force-length_model_definition","xs:string");
+  forceVelocityModelDef->add("constant");
+  forceVelocityModelDef->add("linear");
+  forceVelocityModelDef->add("hill");
+  spec.add(forceVelocityModelDef);
+}
