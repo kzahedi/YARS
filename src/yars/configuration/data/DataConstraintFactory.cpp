e529f481e67f0588aba04c51b112cda74b2cb78e
Keyan <keyan.zahedi@gmail.com>
2017-11-14 11:02:29 +0100

    Some minor changes + YARS_CPP_CLIENT default on ON

--- src/yars/configuration/data/DataConstraintFactory.cpp
+++
@@ -1,8 +0,0 @@
-#include "DataConstraintFactory.h"
-
-
-void DataConstraintFactory::createXsd(XsdSpecification *spec)
-{
-  // XsdSequence *constraints = new XsdSequence(YARS_STRING_CONSTRAINTS_DEFINITION);
-  // spec->add(constraints);
-}
